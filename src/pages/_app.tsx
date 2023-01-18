7bdebe53873b2f54905b29a5f1e060d9b7b0394e
seijinrosen <86702775+seijinrosen@users.noreply.github.com>
2023-01-19 02:32:47 +0900

    Add chakra provider

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,12 @@
-import "@/styles/globals.css";
+// https://chakra-ui.com/getting-started/nextjs-guide#provider-setup
+
+import { ChakraProvider } from "@chakra-ui/react";
 import type { AppProps } from "next/app";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <ChakraProvider>
+      <Component {...pageProps} />
+    </ChakraProvider>
+  );
 }
