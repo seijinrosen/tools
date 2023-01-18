17f28b36cf3b49eab9ccede38c10f7d71125013e
seijinrosen <86702775+seijinrosen@users.noreply.github.com>
2023-01-18 13:11:54 +0900

    Create hello-world page

---
+++ src/pages/hello-page.tsx
@@ -0,0 +1,12 @@
+import HelloWorld from "@/components/HelloWorld";
+import { NextPage } from "next";
+
+const HelloPage: NextPage = () => {
+  return (
+    <>
+      <HelloWorld />
+    </>
+  );
+};
+
+export default HelloPage;
