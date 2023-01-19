1ffaf42527dfc72d4f8b3fdb1c1c37ecabd2a898
seijinrosen <86702775+seijinrosen@users.noreply.github.com>
2023-01-19 18:06:07 +0900

    Move theme.ts

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -2,7 +2,7 @@
 
 import { ColorModeScript } from "@chakra-ui/react";
 import { Head, Html, Main, NextScript } from "next/document";
-import theme from "./theme";
+import theme from "../theme";
 
 export default function Document() {
   return (
