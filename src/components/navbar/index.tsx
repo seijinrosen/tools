09dc6742c89d4e58e6698d79a7fe83876a3e3e82
seijinrosen <86702775+seijinrosen@users.noreply.github.com>
2023-01-20 22:12:57 +0900

    Update Navbar

--- src/components/navbar/index.tsx
+++ src/components/navbar/index.tsx
@@ -1,28 +1,53 @@
 // https://nextjs.org/docs/api-reference/next/link
+// https://chakra-templates.dev/navigation/navbar
 
+import { CloseIcon, HamburgerIcon } from "@chakra-ui/icons";
+import {
+  Box,
+  Flex,
+  IconButton,
+  Stack,
+  useColorModeValue,
+  useDisclosure,
+} from "@chakra-ui/react";
 import Link from "next/link";
 import ColorToggleButton from "../ColorToggleButton";
 
-function Navbar() {
+const Navbar = () => {
+  const { isOpen, onOpen, onClose } = useDisclosure();
+
   return (
-    <header>
-      <ul>
-        <li>
-          <Link href="/">Home</Link>
-        </li>
-        <li>
-          <Link href="/clipboard">clipboard</Link>
-        </li>
-        <li>
-          <Link href="/hello-page">hello-page</Link>
-        </li>
-        <li>
-          <Link href="/nextjs">nextjs</Link>
-        </li>
-      </ul>
-      <ColorToggleButton />
-    </header>
+    <Box as="header" bg={useColorModeValue("gray.100", "gray.900")} px={4}>
+      <Flex h={16} alignItems={"center"} justifyContent={"space-between"}>
+        <IconButton
+          icon={isOpen ? <CloseIcon /> : <HamburgerIcon />}
+          aria-label={"Open Menu"}
+          onClick={isOpen ? onClose : onOpen}
+        />
+        <Box>tools</Box>
+        <ColorToggleButton />
+      </Flex>
+
+      {isOpen ? (
+        <Stack as={"nav"} spacing={4}>
+          <ul>
+            <li>
+              <Link href="/">Home</Link>
+            </li>
+            <li>
+              <Link href="/clipboard">clipboard</Link>
+            </li>
+            <li>
+              <Link href="/hello-page">hello-page</Link>
+            </li>
+            <li>
+              <Link href="/nextjs">nextjs</Link>
+            </li>
+          </ul>
+        </Stack>
+      ) : null}
+    </Box>
   );
-}
+};
 
 export default Navbar;
