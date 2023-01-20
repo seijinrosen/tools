b48cc8aae423f1e21ea87ee0686aa73668d2ddd6
seijinrosen <86702775+seijinrosen@users.noreply.github.com>
2023-01-20 20:43:53 +0900

    Add simple index page

---
+++ src/pages/index.tsx
@@ -0,0 +1,14 @@
+import Head from "next/head";
+
+const Home = () => {
+  return (
+    <>
+      <Head>
+        <title>Home | tools</title>
+      </Head>
+      <main>ホーム</main>
+    </>
+  );
+};
+
+export default Home;
