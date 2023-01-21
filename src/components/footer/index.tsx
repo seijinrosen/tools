8cb8867d977f61725147cbd496033540a4efaedb
seijinrosen <86702775+seijinrosen@users.noreply.github.com>
2023-01-21 18:15:29 +0900

    Update Footer

--- src/components/footer/index.tsx
+++ src/components/footer/index.tsx
@@ -1,11 +1,20 @@
-import { Link, Text, VStack } from "@chakra-ui/react";
+import { Link, Stack, Text, VStack } from "@chakra-ui/react";
+import { FaGithub, FaTwitter } from "react-icons/fa";
 
 const Footer = () => {
   return (
     <VStack as="footer" mt="auto" mb={12}>
+      <Stack mt={4} direction="row" spacing={6}>
+        <Link href="https://github.com/seijinrosen/tools" isExternal>
+          <FaGithub fontSize={20} />
+        </Link>
+        <Link href="https://twitter.com/seijinrosen" isExternal>
+          <FaTwitter fontSize={20} color="#1DA1F2" />
+        </Link>
+      </Stack>
       <Text>
         Copyright ©{" "}
-        <Link href="https://seijinrosen.github.io/" isExternal>
+        <Link color="teal.500" href="https://seijinrosen.github.io" isExternal>
           seijinrosen
         </Link>{" "}
         {new Date().getFullYear()}.
